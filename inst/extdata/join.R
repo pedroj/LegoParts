51c96662abac41001d3060f7e7653e0fc9a24bbc
Stephen Kaluzny <spkal@washington.edu>
2018-01-08 22:18:45 -0800

    Further join work.

--- inst/extdata/join.R
+++ inst/extdata/join.R
@@ -6,3 +6,12 @@ names(dleft)
 ## [1] "inventory_id" "part_num"     "color_id"     "quantity.x"   "is_spare"
 ## [6] "set_num"      "quantity.y"
 dleft <- left_join(sets, inventory_sets, by="set_num")
+
+
+dleft <- left_join(inventory_parts, inventories, by=c("inventory_id" = "id"))
+dim(dleft)
+## [1] 612618      7
+names(dleft)
+## [1] "inventory_id" "part_num"     "color_id"     "quantity"     "is_spare"
+## [6] "version"      "set_num"
+
