57a4185209be002287370f1f22f495ffc4d4d300
spkal <spk@seapowet.org>
2018-01-11 06:38:22 -0800

    Further joins of the tables.

--- inst/extdata/join.R
+++ inst/extdata/join.R
@@ -8,10 +8,16 @@ names(dleft)
 dleft <- left_join(sets, inventory_sets, by="set_num")
 
 
-dleft <- left_join(inventory_parts, inventories, by=c("inventory_id" = "id"))
-dim(dleft)
+dleft01 <- left_join(inventory_parts, inventories, by=c("inventory_id" = "id"))
+dim(dleft01)
 ## [1] 612618      7
-names(dleft)
+names(dleft01)
 ## [1] "inventory_id" "part_num"     "color_id"     "quantity"     "is_spare"
 ## [6] "version"      "set_num"
 
+dleft02 <- left_join(dleft01, parts, by="part_num")
+dim(dleft02)
+## [1] 612618      9
+names(dleft02)
+## [1] "inventory_id" "part_num"     "color_id"     "quantity"     "is_spare"
+## [6] "version"      "set_num"      "name"         "part_cat_id"
